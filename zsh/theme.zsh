0298c5f7aaf4ec0f4418733f4092e99f8371000a
Fredrik Frodlund <fredrik.frodlund@jayway.com>
2015-03-24 20:09:26 +0100

    Make git time since commit message readable

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -93,7 +93,7 @@ ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[grey]%}◒ "
 ZSH_THEME_GIT_TIME_SINCE_COMMIT_SHORT="%{$fg[green]%}"
 ZSH_THEME_GIT_TIME_SHORT_COMMIT_MEDIUM="%{$fg[yellow]%}"
 ZSH_THEME_GIT_TIME_SINCE_COMMIT_LONG="%{$fg[red]%}"
-ZSH_THEME_GIT_TIME_SINCE_COMMIT_NEUTRAL="%{$fg[grey]%}"
+ZSH_THEME_GIT_TIME_SINCE_COMMIT_NEUTRAL="%{$fg[white]%}"
 
 # LS colors
 export LSCOLORS="exfxcxdxbxegedabagacad"
