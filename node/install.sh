ae4372ccce07a84d7c5f04652293b8e4b8250ac9
Fredrik Frodlund <fredrik.frodlund@jayway.com>
2015-03-24 20:57:43 +0100

    Update npm package install script

--- node/install.sh
+++ node/install.sh
@@ -6,13 +6,13 @@
 # using Homebrew.
 
 # Check for npm
-if test ! $(which npm)
+if test $(which npm)
 then
-  echo "Sorry! You need to install node first."
-  exit
-else
   # Install npm packages globally
   npm install -g autoprefixer bower csscomb grunt http-server live-server nodemon nvm
+else
+  echo "Sorry! You need to install node first."
+  exit 1
 fi
 
 exit 0
