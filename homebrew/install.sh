d183f80505e9c5d788f764ea1c22eb07b62ea4b4
Fredrik Frodlund <fredrik.frodlund@jayway.com>
2015-03-24 20:13:13 +0100

    Use an array for homebrew packages for better readability
    Props to @jlowgren for this nice little thing! 😊

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-#
-# Homebrew
-#
-# This installs some of the common dependencies needed (or at least desired)
-# using Homebrew.
 
 # Check for Homebrew
 if test ! $(which brew)
@@ -12,7 +7,28 @@ then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Install homebrew packages
-brew install archey bash-completion coreutils gem-completion git grc htop-osx mysql nginx node php54 ruby-completion tmux tree unrar wget youtube-dl
+# Packages to install
+packages=(
+  "archey"
+  "bash-completion"
+  "coreutils"
+  "gem-completion"
+  "git"
+  "grc"
+  "htop-osx"
+  "mysql"
+  "nginx"
+  "node"
+  "php54"
+  "ruby-completion"
+  "tmux"
+  "tree"
+  "unrar"
+  "wget"
+  "youtube-dl"
+)
+
+# Install
+brew install $(IFS=" "; echo "${packages[*]}")
 
 exit 0
