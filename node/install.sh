16a3cff61bc84e8b0b32fddb0f377fa094eddd24
Fredrik Frodlund <fredrik.frodlund@jayway.com>
2015-03-24 14:49:30 +0100

    Add node config with npm install script

---
+++ node/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+# Check for npm
+if test ! $(which npm)
+then
+  echo "Sorry! You need to install node first."
+  exit
+else
+  # Install npm packages globally
+  npm install -g autoprefixer bower csscomb grunt hs http-server live-server nodemon nvm
+fi
+
+exit 0
