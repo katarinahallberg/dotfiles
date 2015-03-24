14b743c19eb7e202ed17165e47989d2defc27c36
Fredrik Frodlund <frippz@me.com>
2015-03-24 22:56:38 +0100

    Fix array syntax error

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,10 +9,10 @@ fi
 
 # Taps to… tap before installing
 taps=(
-  homebrew/completions
-  homebrew/dupes
-  homebrew/php
-  homebrew/versions
+  "homebrew/completions"
+  "homebrew/dupes"
+  "homebrew/php"
+  "homebrew/versions"
 )
 
 # Tap it!
