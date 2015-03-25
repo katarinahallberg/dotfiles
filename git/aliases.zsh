1869351a2113e0c6febd3768849de99dee7d83bc
Fredrik Frodlund <frippz@me.com>
2015-03-25 08:31:17 +0100

    Remove commented aliases that we won't be using

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -177,16 +177,3 @@ alias gignore='git update-index --assume-unchanged'
 alias gunignore='git update-index --no-assume-unchanged'
 # list temporarily ignored files
 alias gignored='git ls-files -v | grep "^[[:lower:]]"'
-
-
-# The rest of my fun git aliases
-# alias gl='git pull --prune'
-# alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-# alias gp='git push origin HEAD'
-# alias gd='git diff'
-# alias gc='git commit'
-# alias gca='git commit -a'
-# alias gco='git checkout'
-# alias gcb='git copy-branch-name'
-# alias gb='git branch'
-# alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
