bd6976f5ea8ae7c5e48f734a07caddc09f746ba5
Fredrik Frodlund <frippz@me.com>
2015-03-30 08:42:10 +0200

    Update $PATH

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,3 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+#export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="$HOME/.rvm/bin:/usr/local/bin:/usr/local/sbin:$HOME/Scripts:/usr/local/share/npm/bin:$(brew --prefix homebrew/php/php54)/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
