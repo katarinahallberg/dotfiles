1214b73ceabd050b04d1d1d2d39e0c6a311a28c1
Fredrik Frodlund <frippz@me.com>
2015-04-07 07:58:05 +0200

    Check if we've got Homebrew before setting $PATH

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,8 @@
-export PATH="$HOME/.rvm/bin:/usr/local/bin:/usr/local/sbin:$HOME/Scripts:/usr/local/share/npm/bin:$(brew --prefix homebrew/php/php54)/bin:$PATH"
+# Check if we've got Homebrew before setting $PATH
+if [ -x "$(command -v brew)" ]; then
+  export PATH="$HOME/.rvm/bin:/usr/local/bin:/usr/local/sbin:$HOME/Scripts:/usr/local/share/npm/bin:$(brew --prefix homebrew/php/php54)/bin:$PATH"
+else
+  export PATH="$HOME/.rvm/bin:/usr/local/bin:/usr/local/sbin:$HOME/Scripts:/usr/local/share/npm/bin:/bin:$PATH"
+fi
+
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
