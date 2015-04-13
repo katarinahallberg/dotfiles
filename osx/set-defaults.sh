928f60c616ac38f3c15f0316fe71da88d13ec0f0
Fredrik Frodlund <frippz@me.com>
2015-04-13 12:44:10 +0200

    Automatically quit printer app once the print jobs complete

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -20,3 +20,6 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
