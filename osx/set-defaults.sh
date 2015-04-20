b963fbee26add0ebf93b7b5540132e94e1ee137b
Fredrik Frodlund <frippz@me.com>
2015-04-20 10:03:54 +0200

    Add a few more OS X defaults

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -23,3 +23,11 @@ defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
 # Automatically quit printer app once the print jobs complete
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Use scroll gesture with the Ctrl (^) modifier key to zoom
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
+
+# Follow the keyboard focus while zoomed in
+defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
+
