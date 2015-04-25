9621258ffce7c5bab26c34700659b57b42fac640
Fredrik Frodlund <frippz@me.com>
2015-04-25 07:48:49 +0200

    Move npm packages into array

--- node/install.sh
+++ node/install.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 #
-# Homebrew
+# Node package manager (NPM)
 #
-# This installs some of the common dependencies needed (or at least desired)
-# using Homebrew.
 
 # Check for npm
 if test $(which npm)
 then
   # Install npm packages globally
-  npm install -g autoprefixer bower csscomb grunt http-server live-server nodemon nvm localtunnel
+  npm install $(IFS=" "; echo "${packages[*]}")
 else
   echo "Sorry! You need to install node first."
   exit 1
 fi
+
+# Packages to install
+Packages=(
+  "autoprefixer"
+  "bower"
+  "csscomb"
+  "grunt"
+  "http-server"
+  "live-server"
+  "nodemon"
+  "nvm"
+  "localtunnel"
+)
 
 exit 0
