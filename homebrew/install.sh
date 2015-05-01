ab379c1a179413a287424456d4f1a7d2e8d4caa7
Fredrik Frodlund <frippz@me.com>
2015-05-01 11:20:08 +0200

    Don't try to tap of install if Homebrew isn't available

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -45,19 +45,23 @@ if uname | grep -q "Darwin" ; then
 fi
 
 # Tap the taps (unless we already have them tapped)
-echo "Checking for any taps to tap..."
-for TAP in ${TAPS[@]} ; do
-  if ! brew tap | grep -q "^${TAPS}\$" ; then
-    brew tap $TAP
-  fi
-done
+if test $(which brew) ; then
+    echo "Checking for any taps to tap..."
+  for TAP in ${TAPS[@]} ; do
+    if ! brew tap | grep -q "^${TAPS}\$" ; then
+      brew tap $TAP
+    fi
+  done
+fi
 
 # Install if packages are not already installed
-echo "Checking for packages to install..."
-for PACKAGE in ${PACKAGES[@]} ; do
-  if ! brew list -1 | grep -q "^${PACKAGE}\$" ; then
-    brew install $PACKAGE
-  fi
-done
+if test $(which brew) ; then
+  echo "Checking for packages to install..."
+  for PACKAGE in ${PACKAGES[@]} ; do
+    if ! brew list -1 | grep -q "^${PACKAGE}\$" ; then
+      brew install $PACKAGE
+    fi
+  done
+fi
 
 exit 0
