df0859429f61b6e051c01a4b500c86104c3c6b32
Fredrik Frodlund <frippz@me.com>
2015-05-01 11:16:39 +0200

    Check if we're on Darwin before checking for Homebrew

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -37,9 +37,11 @@ PACKAGES=(
 )
 
 # Check for Homebrew
-if test ! $(which brew) && uname === "Darwin" ; then
-  echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if uname | grep -q "Darwin" ; then
+  if test ! $(which brew) ; then
+    echo "  Installing Homebrew for you."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
 fi
 
 # Tap the taps (unless we already have them tapped)
