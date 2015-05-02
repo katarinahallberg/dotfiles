a138e4290c337765565be0d7f7ad6df3cdeb3793
Fredrik Frodlund <frippz@me.com>
2015-05-02 13:12:44 +0200

    Single quote instead of double

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,18 +1,18 @@
 # Some nice aliases
-alias ls="ls -GF"
-alias l="ls -GF"
-alias ll="ls -lh"
-alias la="ls -lha"
-alias c="clear"
-alias x="exit"
-alias df="gdf -H"
-alias grep="grep --color"
+alias ls='ls -GF'
+alias l='ls -GF'
+alias ll='ls -lh'
+alias la='ls -lha'
+alias c='clear'
+alias x='exit'
+alias df='gdf -H'
+alias grep='grep --color'
 
-# Clean up "Open with…" menu
-alias fixopenwith="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user"
+# Clean up 'Open with…' menu
+alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
 
 # Nice looking time stamp
-alias tstamp="date +%Y%m%d%H%M"
+alias tstamp='date +%Y%m%d%H%M'
 
 # zsh
 alias reload!='. ~/.zshrc'
