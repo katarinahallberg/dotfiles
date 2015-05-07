95a5508d8a44443b74c065a798868c343f15c880
Fredrik Frodlund <frippz@me.com>
2015-05-07 08:34:40 +0200

    Don't runt compdef for tmuxinator if we don't have it installed

--- tmuxinator/tmuxinator.zsh
+++ tmuxinator/tmuxinator.zsh
@@ -1,30 +1,34 @@
 #compdef tmuxinator mux
 
-_tmuxinator() {
-  local commands projects
-  commands=(${(f)"$(tmuxinator commands zsh)"})
-  projects=(${(f)"$(tmuxinator completions start)"})
+if test $(which tmuxinator) ; then
 
-  if (( CURRENT == 2 )); then
-    _describe -t commands "tmuxinator subcommands" commands
-    _describe -t projects "tmuxinator projects" projects
-  elif (( CURRENT == 3)); then
-    case $words[2] in
-      copy|debug|delete|open|start)
-        _arguments '*:projects:($projects)'
-      ;;
-    esac
-  fi
+  _tmuxinator() {
+    local commands projects
+    commands=(${(f)"$(tmuxinator commands zsh)"})
+    projects=(${(f)"$(tmuxinator completions start)"})
 
-  return
-}
+    if (( CURRENT == 2 )); then
+      _describe -t commands "tmuxinator subcommands" commands
+      _describe -t projects "tmuxinator projects" projects
+    elif (( CURRENT == 3)); then
+      case $words[2] in
+        copy|debug|delete|open|start)
+          _arguments '*:projects:($projects)'
+        ;;
+      esac
+    fi
 
-_tmuxinator
+    return
+  }
 
-# Local Variables:
-# mode: Shell-Script
-# sh-indentation: 2
-# indent-tabs-mode: nil
-# sh-basic-offset: 2
-# End:
-# vim: ft=zsh sw=2 ts=2 et
+  _tmuxinator
+
+  # Local Variables:
+  # mode: Shell-Script
+  # sh-indentation: 2
+  # indent-tabs-mode: nil
+  # sh-basic-offset: 2
+  # End:
+  # vim: ft=zsh sw=2 ts=2 et
+
+fi
