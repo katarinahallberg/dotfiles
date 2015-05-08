3020d0501607fdc35f8f66f4b61c1ffe68a2bdfa
Fredrik Frodlund <frippz@me.com>
2015-05-08 13:37:06 +0200

    Remove rvm

--- rvm/rvm.zsh
+++
@@ -1,4 +0,0 @@
-# Load RVM into a shell session as a function
-if [[ -f "$HOME/.rvm/scripts/rvm" ]]; then
-  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-fi
--- system/_path.zsh
+++ system/_path.zsh
@@ -1,8 +1,8 @@
 # Check if we've got Homebrew before setting $PATH
-if [ -x "$(command -v brew)" ]; then
-  export PATH="$HOME/.rvm/bin:/usr/local/bin:/usr/local/sbin:$HOME/.scripts:/usr/local/share/npm/bin:$(brew --prefix homebrew/php/php54)/bin:$PATH"
+if [ -x "$(command -v brew)" ] ; then
+  export PATH="/usr/local/bin:/usr/local/sbin:$HOME/.scripts:/usr/local/share/npm/bin:$(brew --prefix homebrew/php/php54)/bin:$PATH"
 else
-  export PATH="$HOME/.rvm/bin:/usr/local/bin:/usr/local/sbin:$HOME/.scripts:/usr/local/share/npm/bin:/bin:$PATH"
+  export PATH="/usr/local/bin:/usr/local/sbin:$HOME/.scripts:/usr/local/share/npm/bin:/bin:$PATH"
 fi
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
