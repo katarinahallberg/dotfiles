13698552a1683ce8fff16781bb65ccaaaa013d99
Fredrik Frodlund <frippz@me.com>
2015-05-10 10:09:06 +0200

    Use single quotes

--- ruby/install.sh
+++ ruby/install.sh
@@ -4,36 +4,37 @@
 
 set -e
 
-RUBY_VERSION="2.2.2"
+RUBY_VERSION='2.2.2'
 
 GEMS=(
-  "bundler"
-  "jekyll"
-  "tmuxinator"
+  'bundler'
+  'jekyll'
+  'tmuxinator'
 )
 
 if ! $(command -v rbenv >/dev/null 2>&1) ; then
-  echo "  Installing rbenv for you."
+  echo '  Installing rbenv for you.'
   brew install rbenv > /tmp/rbenv-install.log
 fi
 
 if ! $(command -v ruby-build >/dev/null 2>&1) ; then
-  echo "  Installing ruby-build and rbenv-gem-rehash for you."
+  echo '  Installing ruby-build and rbenv-gem-rehash for you.'
   brew install ruby-build > /tmp/ruby-build-install.log
   brew install rbenv-gem-rehash > /tmp/rbenv-gem-rehash-install.log
 fi
 
 # Check for rbenv before attempting to install gems
 if $(command -v rbenv >/dev/null 2>&1) ; then
+  # Check if our selected version of Ruby is installed
   if ! [[ $(rbenv global) == ${RUBY_VERSION} ]] ; then
-    echo 'Installing Ruby for you.'
+    echo ' Installing Ruby for you.'
     rbenv install ${RUBY_VERSION}
     rbenv global ${RUBY_VERSION}
   fi
   gem update --system
-  echo "Checking for gems to install"
+  echo '  Checking for gems to install.'
   for GEM in ${GEMS[@]} ; do
-    if ! gem list | grep -q "${GEM}" ; then
+    if ! gem list | grep -q '${GEM}' ; then
       gem install $GEM
     fi
   done
