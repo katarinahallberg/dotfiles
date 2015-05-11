4669180bea765c9c48d27834edb29c136977499c
Fredrik Frodlund <frippz@me.com>
2015-05-11 09:45:06 +0200

    Add notification in install.sh for Node

--- node/install.sh
+++ node/install.sh
@@ -4,6 +4,10 @@
 
 set -e
 
+echo ""
+echo "Running Node install script"
+echo ""
+
 # Packages to install
 PACKAGES=(
   "autoprefixer"
@@ -19,6 +23,7 @@ PACKAGES=(
 
 # Check for npm
 if test $(which npm) ; then
+  echo "  Looks like npm is installed. Checking for packages to install."
   # Install npm packages globally
   for PACKAGE in ${PACKAGES[@]} ; do
     if ! npm -g list | grep -q "${PACKAGE}" ; then
