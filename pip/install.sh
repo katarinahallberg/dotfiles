c1517bcf3ac282b140e8dcf1ee7dbf5e7843e0c6
Fredrik Frodlund <frippz@me.com>
2015-05-11 09:45:14 +0200

    Add notification in install.sh for pip

--- pip/install.sh
+++ pip/install.sh
@@ -1,4 +1,11 @@
 # Install pygments and then set the ccat alias
+
+set -e
+
+echo ""
+echo "Running pip install script"
+echo ""
+
 if [ -x "$(command -v pip)" ]; then
 
   #Install Pygments (or try upgrade if it is already installed)
