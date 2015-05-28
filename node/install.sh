5a7a0ea146403c2cf4d5c3bce43425956fadec7f
Fredrik Frodlund <frippz@me.com>
2015-05-28 10:18:14 +0200

    Update verbosity of Node install script

--- node/install.sh
+++ node/install.sh
@@ -18,16 +18,19 @@ PACKAGES=(
 )
 
 echo ""
-echo "Running Node install script"
+echo "  Running Node install script"
 echo ""
 
 # Check for npm
 if test $(which npm) ; then
   echo "  Looks like npm is installed. Checking for packages to install."
+  echo ""
   # Install npm packages globally
   for PACKAGE in ${PACKAGES[@]} ; do
     if ! npm -g list | grep -q "${PACKAGE}" ; then
       npm install -g $PACKAGE
+    else
+      echo "    * Found ${PACKAGE}"
     fi
   done
 fi
