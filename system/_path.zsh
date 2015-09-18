6b9a5bbeb4dcc51db22bd874349a373b8f7be5f0
Fredrik Frodlund <frippz@me.com>
2015-09-18 11:39:06 +0200

    Update path to reflect new PHP version

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,6 +1,6 @@
 # Check if we've got Homebrew before setting $PATH
 if [ -x "$(command -v brew)" ] ; then
-  export PATH="/usr/local/bin:/usr/local/sbin:$HOME/.scripts:/usr/local/share/npm/bin:$(brew --prefix homebrew/php/php54)/bin:$PATH"
+  export PATH="/usr/local/bin:/usr/local/sbin:$HOME/.scripts:/usr/local/share/npm/bin:$(brew --prefix homebrew/php/php56)/bin:$PATH"
 else
   export PATH="/usr/local/bin:/usr/local/sbin:$HOME/.scripts:/usr/local/share/npm/bin:/bin:$PATH"
 fi
