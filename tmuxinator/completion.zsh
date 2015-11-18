eeb7b227a5743345aaf724329c973a00b85f2685
Fredrik Frodlund <frippz@me.com>
2015-11-18 14:10:17 +0100

    Re-indent completion file for tmuxinator

--- tmuxinator/completion.zsh
+++ tmuxinator/completion.zsh
@@ -5,21 +5,21 @@ if [[ -n ${ZSH_VERSION-} ]]; then
 fi
 
 _tmuxinator() {
-    COMPREPLY=()
-    local word="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=()
+  local word="${COMP_WORDS[COMP_CWORD]}"
 
-    if [ "$COMP_CWORD" -eq 1 ]; then
-        local commands="$(compgen -W "$(tmuxinator commands)" -- "$word")"
-        local projects="$(compgen -W "$(tmuxinator completions start)" -- "$word")"
+  if [ "$COMP_CWORD" -eq 1 ]; then
+    local commands="$(compgen -W "$(tmuxinator commands)" -- "$word")"
+    local projects="$(compgen -W "$(tmuxinator completions start)" -- "$word")"
 
-        COMPREPLY=( $commands $projects )
-    else
-        local words=("${COMP_WORDS[@]}")
-        unset words[0]
-        unset words[$COMP_CWORD]
-        local completions=$(tmuxinator completions "${words[@]}")
-        COMPREPLY=( $(compgen -W "$completions" -- "$word") )
-    fi
+    COMPREPLY=( $commands $projects )
+  else
+    local words=("${COMP_WORDS[@]}")
+    unset words[0]
+    unset words[$COMP_CWORD]
+    local completions=$(tmuxinator completions "${words[@]}")
+    COMPREPLY=( $(compgen -W "$completions" -- "$word") )
+  fi
 }
 
 complete -F _tmuxinator tmuxinator mux
