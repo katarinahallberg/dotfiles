3399a209f8bd08c489e35c19e85ff316bd0bc1fa
Fredrik Frodlund <frippz@me.com>
2015-12-14 13:52:11 +0100

    Add node config for Linux as well

--- node/node.zsh
+++ node/node.zsh
@@ -1,6 +1,10 @@
 # Node environment config
-export NODE_PATH="/usr/local/lib/node"
+if [[ "$(uname -s)" == "Darwin" ]] ; then
+  export NODE_PATH="/usr/local/lib/node"
+elif [[ "$(uname -s)" == "Linux" ]] ; then
+  export NODE_PATH="/usr/bin/node"
+fi
 
 # nvm
-export NVM_DIR="/Users/fredrik/.nvm"
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
