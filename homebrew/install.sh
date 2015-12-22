6abc5113bb8a1e91c5675d84843857cd577b14e0
Fredrik Frodlund <frippz@me.com>
2015-12-22 15:51:03 +0100

    Remove old Homebrew install script and use Bundle instead

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,118 +1,18 @@
 #!/usr/bin/env bash
 #
-# Setup required taps and install Homebrew packages
-
-# Continue on errors
-set +e
+# Setup required taps and install Homebrew packages using Brewfile
 
 if command -v brew >/dev/null 2>&1 ; then
-  # Cache list of already installed eackages
-  INSTALLED_PACKAGES=( $(brew list -1) )
 
-  # Cache taps as well
-  TAPPED_TAPS=( $(brew tap) )
-fi
-
-# Set up taps before installing
-TAPS=(
-  "homebrew/completions"
-  "homebrew/dupes"
-  "homebrew/php"
-  "homebrew/versions"
-  "neovim/neovim"
-  "yudai/gotty"
-)
-
-# Packages to install
-PACKAGES=(
-  "ack"
-  "ccat"
-  "coreutils"
-  "dos2unix"
-  "editorconfig"
-  "gem-completion"
-  "git"
-  "gotty"
-  "grc"
-  "htop-osx"
-  "imagemagick"
-  "macvim"
-  "mysql"
-  "neovim"
-  "nginx"
-  "node"
-  "php56"
-  "php56-mcrypt"
-  "python"
-  "rbenv"
-  "rbenv-gem-rehash"
-  "rbenv-gemset"
-  "ruby-build"
-  "ruby-completion"
-  "tidy-html5"
-  "tmux"
-  "tree"
-  "unrar"
-  "unzip"
-  "vim"
-  "wget"
-  "youtube-dl"
-  "zsh"
-  "zsh-syntax-highlighting"
-)
-
-# Packages to remove
-RM_PACKAGES=(
-  "zsh-completions"
-)
-
-# TODO vim and macvim needs these as flags: " --with-lua --without-python --with-python3"
-
-echo ""
-echo "  √ Running Homebrew install script"
-
-# Check if we're on Darwin, then check for Homebrew
-if [[ "$(uname -s)" == "Darwin" ]] ; then
-  if ! command -v brew >/dev/null 2>&1 ; then
-    echo "  Installing Homebrew for you."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  # Tap Brewfile and run brew bundle
+  if ! echo "brew tap" | grep -q "homebrew/bundle" ; then
+    echo ""
+    echo "  √ Running Homebrew bundle script"
+    echo ""
+    brew tap homebrew/bundle
+    brew bundle
   fi
-fi
 
-# Set up taps
-if command -v brew >/dev/null 2>&1 ; then
-  echo ""
-  echo "  √ Checking for anything to tap..."
-  for TAP in ${TAPS[@]} ; do
-    if ! echo "${TAPPED_TAPS[@]}" | grep -q "${TAP}" ; then
-      brew tap $TAP
-    fi
-  done
 fi
-
-# Remove packages (do this before attempting to install to prevent conflicts)
-if command -v brew >/dev/null 2>&1 ; then
-  echo ""
-  echo "  √ Checking for packages to remove..."
-  for RM_PACKAGE in ${RM_PACKAGES[@]} ; do
-    if echo "${INSTALLED_PACKAGES[@]}" | grep -q "${RM_PACKAGE}" ; then
-      brew uninstall $RM_PACKAGE
-    fi
-  done
-fi
-
-# Install packages
-if command -v brew >/dev/null 2>&1 ; then
-  echo ""
-  echo "  √ Checking for packages to install..."
-  for PACKAGE in ${PACKAGES[@]} ; do
-    if ! echo "${INSTALLED_PACKAGES[@]}" | grep -q "${PACKAGE}" ; then
-      brew install $PACKAGE
-    fi
-  done
-fi
-
-# Resume halt on error
-set -e
 
 exit 0
