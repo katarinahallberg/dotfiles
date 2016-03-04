93112b309d75b1ae4153dfba6d68d79d6c94517d
Fredrik Frodlund <frippz@me.com>
2016-03-04 17:14:34 +0000

    Check for brew for homebrew aliases

--- homebrew/aliases.zsh
+++ homebrew/aliases.zsh
@@ -1,2 +1,4 @@
-alias brews='brew list --versions'
-alias bubu='brew update && brew upgrade --all && brew cleanup'
+if command -v brew >/dev/null 2>&1 ; then
+  alias brews='brew list --versions'
+  alias bubu='brew update && brew upgrade --all && brew cleanup'
+fi
