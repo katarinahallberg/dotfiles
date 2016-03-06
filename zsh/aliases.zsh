cc4a30a974215e63782d94d0560d2ec4adc651c7
Fredrik Frodlund <frippz@me.com>
2016-03-06 09:44:05 +0100

    Move OS X only aliases to osx folder

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,12 +12,6 @@ if [[ "$(uname -s)" == "Darwin" ]] ; then
     alias cat="ccat"
   fi
 
-  # Clean up "Open with…" menu
-  alias fixopenwith="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user"
-
-  # Start OS X screensaver
-  alias screensaver-launch="open /System/Library/Frameworks/ScreenSaver.framework/Versions/Current/Resources/ScreenSaverEngine.app"
-
 fi
 
 # Linux only aliases
