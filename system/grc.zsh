f6e46e39af10a065e4537a74a91691a59735a246
Fredrik Frodlund <frippz@me.com>
2016-04-21 06:08:13 +0000

    Don’t alias grc if we don’t have it

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,24 +1,25 @@
-GRC=`which grc`
-if [ "$TERM" != dumb ] && [ -n "$GRC" ]
-then
-    alias colourify="$GRC -es --colour=auto"
-    alias configure='colourify ./configure'
-    alias diff='colourify diff'
-    alias make='colourify make'
-    alias gcc='colourify gcc'
-    alias g++='colourify g++'
-    alias as='colourify as'
-    alias gas='colourify gas'
-    alias ld='colourify ld'
-    alias netstat='colourify netstat'
-    alias ping='colourify ping'
-    alias traceroute='colourify /usr/sbin/traceroute'
-    alias head='colourify head'
-    alias tail='colourify tail'
-    alias dig='colourify dig'
-    alias mount='colourify mount'
-    alias ps='colourify ps'
-    alias mtr='colourify mtr'
-    alias df='colourify df -H'
+if command -v grc >/dev/null 2>&1 ; then
+  GRC=`which grc`
+  if [ "$TERM" != dumb ] && [ -n "$GRC" ]
+  then
+      alias colourify="$GRC -es --colour=auto"
+      alias configure='colourify ./configure'
+      alias diff='colourify diff'
+      alias make='colourify make'
+      alias gcc='colourify gcc'
+      alias g++='colourify g++'
+      alias as='colourify as'
+      alias gas='colourify gas'
+      alias ld='colourify ld'
+      alias netstat='colourify netstat'
+      alias ping='colourify ping'
+      alias traceroute='colourify /usr/sbin/traceroute'
+      alias head='colourify head'
+      alias tail='colourify tail'
+      alias dig='colourify dig'
+      alias mount='colourify mount'
+      alias ps='colourify ps'
+      alias mtr='colourify mtr'
+      alias df='colourify df -H'
+  fi
 fi
-
