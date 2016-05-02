6139ac1557d23c21b6207e8423c3051cd57ddbf2
Fredrik Frodlund <frippz@me.com>
2016-05-02 23:07:31 +0200

    Add rbenv shims & bin to $PATH

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,3 +1,11 @@
+if [ -d $HOME/.rbenv/bin ] ; then
+  export PATH=$HOME/.rbenv/bin
+fi
+
+if [ -d $HOME/.rbenv/shims ] ; then
+  export PATH=$HOME/.rbenv/shims
+fi
+
 # init according to man page
 if (( $+commands[rbenv] )) ; then
   eval "$(rbenv init -)"
