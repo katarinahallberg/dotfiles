9f612f35d43b65fd545cf8e4a4e0fe0111ece8e5
Fredrik Frodlund <frippz@me.com>
2016-05-02 13:47:34 +0200

    Install Homebrew via install script

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -2,6 +2,28 @@
 #
 # Setup required taps and install Homebrew packages using Brewfile
 
+if ! command -v brew >/dev/null 2>&1 ; then
+
+  echo ""
+  echo " √ Installing Homebrew"
+  echo ""
+
+  HOMEBREW_PREFIX="/usr/local"
+
+  if [ -d "$HOMEBREW_PREFIX" ]; then
+    if ! [ -r "$HOMEBREW_PREFIX" ]; then
+      sudo chown -R "$LOGNAME:admin" /usr/local
+    fi
+  else
+    sudo mkdir "$HOMEBREW_PREFIX"
+    sudo chflags norestricted "$HOMEBREW_PREFIX"
+    sudo chown -R "$LOGNAME:admin" "$HOMEBREW_PREFIX"
+  fi
+
+  curl -fsS 'https://raw.githubusercontent.com/Homebrew/install/master/install' | ruby
+
+fi
+
 if command -v brew >/dev/null 2>&1 ; then
 
   # Tap Brewfile
