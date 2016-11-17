ea64a8cceb26d1b382971500cc0596d3d2afe203
Fredrik Frodlund <frippz@me.com>
2016-11-17 15:04:21 +0100

    Add scripted pip upgrade

--- python/install.sh
+++ python/install.sh
@@ -12,6 +12,10 @@ echo ""
 
 # Check for rbenv before attempting to install gems
 if command -v pip3 >/dev/null 2>&1 ; then
+  echo ""
+  echo " √ Upgrading pip3"
+  echo ""
+  pip install --upgrade pip3
   for PIP in ${PIPS[@]} ; do
     pip3 install -U $PIP
   done
@@ -27,11 +31,15 @@ echo ""
 
 # Check for rbenv before attempting to install gems
 if command -v pip2 >/dev/null 2>&1 ; then
+  echo ""
+  echo " √ Upgrading pip2"
+  echo ""
+  pip install --upgrade pip2
   for PIP in ${PIPS[@]} ; do
     pip2 install -U $PIP
   done
 else
   echo ""
-  echo " ✗ pip3 not found. You may need to install python3 first."
+  echo " ✗ pip2 not found. You may need to install python-2.7 first."
   echo ""
 fi
